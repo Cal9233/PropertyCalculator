b7d5dd7d0ab424c214af07f19102dc68d377a1bf
Cal <calvin.m71@hotmail.com>
2024-04-28 19:57:45 -0700

    Added google maps

---
+++ src/Components/Search.jsx
@@ -0,0 +1,10 @@
+const Search = () => {
+    return (
+        <div class="search-container">
+            <input type="text" placeholder="State, City, Zip, Address..." class="search-input" />
+            <button class="search-button">Search</button>
+        </div>
+    );
+}
+
+export default Search;
