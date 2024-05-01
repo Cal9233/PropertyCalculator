2437b8a6672d26eb2c0d4ac76e433d000566fa08
Mohammed Ismail <ikismail7@gmail.com>
2024-05-01 22:10:32 +0530

    fix: :lipstick: Fix GoogleMapNav and Search component imports and function names

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,33 +1,34 @@
 import React, { useState } from "react";
-import SearchIcon from '@mui/icons-material/Search';
-import axios from 'axios';
-import Data from '../Components/Common/Data';
+import SearchIcon from "@mui/icons-material/Search";
+import axios from "axios";
+import Data from "../Components/Common/Data";
 
-const Search = ({ handleOpenModal, setProperties, setLocationData }) => {
+const Search = ({ handleSearchModal, setProperties, setLocationData }) => {
   const [input, setInput] = useState("");
 
   const fetchData = async (value) => {
     try {
       const options = {
-        method: 'GET',
-        url: 'https://zillow56.p.rapidapi.com/search',
+        method: "GET",
+        url: "https://zillow56.p.rapidapi.com/search",
         params: {
-          location: value
+          location: value,
         },
         headers: {
-          'X-RapidAPI-Key': `${process.env.REACT_APP_RAPID_API_KEY}`,
-          'X-RapidAPI-Host': 'zillow56.p.rapidapi.com'
-        }
-      }
+          "X-RapidAPI-Key": `${process.env.REACT_APP_RAPID_API_KEY}`,
+          "X-RapidAPI-Host": "zillow56.p.rapidapi.com",
+        },
+      };
       const response = Data;
       //const response = await axios.request(options); // Assuming the response data has a 'results' property containing the properties
-      console.log(response)
+      console.log("Response is:", response);
       setProperties(response);
       setLocationData(response);
       //handleOpenModal(response.data.results); // Call handleOpenModal with the search results
-      handleOpenModal(response)
+      // handleOpenModal(response)
+      handleSearchModal(response);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   };
 
@@ -36,7 +37,7 @@ const Search = ({ handleOpenModal, setProperties, setLocationData }) => {
   };
 
   const handleKeyDown = (e) => {
-    if (e.key === 'Enter') {
+    if (e.key === "Enter") {
       fetchData(input);
     }
   };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,59 +1,69 @@
-import React, { useState } from 'react';
-import GoogleMapNav from '../Components/GoogleMapNav';
-import Modal from 'react-responsive-modal';
-import Search from '../Components/Search';
+import React, { useState } from "react";
+import GoogleMapNav from "../Components/GoogleMapNav";
+import Modal from "react-responsive-modal";
+import Search from "../Components/Search";
 import { Container } from "@mui/system";
 import { Grid } from "@mui/material";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
 
 const Home = () => {
-  const [modal, setModal] = useState(false);
+  // const [modal, setModal] = useState(false);
   const [selectedProperty, setSelectedProperty] = useState(null);
   const [properties, setProperties] = useState([]);
   const [locationData, setLocationData] = useState(null);
 
+  const handleSearchModal = (property) => {
+    setProperties(property);
+    setSelectedProperty(null);
+    // setModal(true);
+  };
   const handleOpenModal = (property) => {
     setSelectedProperty(property);
-    setModal(true);
+    // setModal(true);
   };
 
   const handleCloseModal = () => {
     setSelectedProperty(null);
-    setModal(false);
+    // setModal(false);
   };
 
   const handleSelectedProperty = (property) => {
     setSelectedProperty(property);
-    setModal(true); 
+    // setModal(true);
   };
 
   return (
     <Container maxWidth="xl">
-    <Grid container spacing={2} alignItems="center">
-      <Grid item xs={12} sx={{ marginTop: 4 }}>
-        <Search
-          handleOpenModal={handleOpenModal}
-          setProperties={setProperties}
-          setLocationData={setLocationData}
-        />
-      </Grid>
-
-      <Grid container spacing={2} alignItems="flex-start">
-        <Grid item xs={selectedProperty ? 8 : 12}>
-          <GoogleMapNav
-            properties={properties}
-            handleOpenModal={handleOpenModal}
-            handleSelectedProperty={handleSelectedProperty}
-            mapCenter={locationData ? locationData.coords : null}
+      <Grid container spacing={0} alignItems="center">
+        <Grid item xs={12} sx={{ marginTop: 2, marginBottom: 2 }}>
+          <Search
+            handleSearchModal={handleSearchModal}
+            setProperties={setProperties}
+            setLocationData={setLocationData}
           />
         </Grid>
-
-        {selectedProperty && (
-          <Grid item xs={4}>
-            <Modal open={modal} onClose={handleCloseModal} styles={{ modal: { right: 0 } }} center>
-              <div className="modal-content">
+        <Grid container spacing={1} alignItems="flex-start">
+          <Grid item xs={7}>
+            <GoogleMapNav
+              properties={properties}
+              handleOpenModal={handleOpenModal}
+              handleSelectedProperty={handleSelectedProperty}
+              mapCenter={locationData ? locationData.coords : null}
+            />
+          </Grid>
+          <Grid item xs={5}>
+            {selectedProperty ? (
+              <div className="property_single">
                 <div className="flex">
                   <div className="w-full">
-                    <img src={selectedProperty.imgSrc} style={{ width: '100px', height: '100px' }} alt="img" />
+                    <img
+                      src={selectedProperty.imgSrc}
+                      style={{ width: "200px", height: "200px" }}
+                      alt="img"
+                    />
                   </div>
                   <div className="w-full ml-3">
                     <h1>{`Price: $${selectedProperty.price}`}</h1>
@@ -61,12 +71,43 @@ const Home = () => {
                   </div>
                 </div>
               </div>
-            </Modal>
+            ) : (
+              <div className="properties">
+                <Grid container columnSpacing={1} rowSpacing={1}>
+                  {(properties || []).map((property, index) => {
+                    return (
+                      <Grid item md={6} key={index}>
+                        <Card sx={{ maxWidth: 300 }}>
+                          <CardMedia
+                            component="img"
+                            height="140"
+                            image={property.imgSrc}
+                            alt="img"
+                          />
+                          <CardContent sx={{ height: 120 }}>
+                            <Typography
+                              gutterBottom
+                              variant="h5"
+                              component="div"
+                            >
+                              {`Price: $${property.price}`}
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                              {property.streetAddress}, {property.state},{" "}
+                              {property.zipcode}
+                            </Typography>
+                          </CardContent>
+                        </Card>
+                      </Grid>
+                    );
+                  })}
+                </Grid>
+              </div>
+            )}
           </Grid>
-        )}
+        </Grid>
       </Grid>
-    </Grid>
-  </Container>
+    </Container>
   );
 };
 
